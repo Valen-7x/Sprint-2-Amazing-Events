0cd4114ac7ab9c49a693d54cff2f0dbdfa0c30b1
Valen-7x <valen.deorigen@gmail.com>
2023-06-04 21:54:02 -0300

    Sprint2-AmzE

---
+++ assets/js/pastEvents.js
@@ -0,0 +1,28 @@
+let card = document.getElementById("box-cards")
+
+function datos(event) {
+return `<div class="cards-css1 card shadow" style="width: 15rem;">
+<img src="${event.image}" class="card-img-top" alt="${event.name}">
+<div class="card-body">
+  <h5 class="card-title">${event.name}</h5>
+  <p class="card-text">${event.description}</p>
+  <div><a href="./pages/museum.html" class="btn btn-primary">Details</a><br><br>
+  <p>Price ${event.price}</p></div>
+</div>
+</div>`;
+  }
+    function imprimirDatos(events) {
+      const fechaActual = (data.currentDate);
+      let template = "";
+      for (let evento of events) {
+        let fechaEvento = (evento.date); 
+        if (fechaEvento < fechaActual) {
+          template += datos(evento);
+        }
+      }
+      
+      console.log(template);
+      card.innerHTML += template;
+    }
+    
+    imprimirDatos(data.events);
